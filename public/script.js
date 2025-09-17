aa8242b3f9e2487fe955d8656716f3e6c5c06216
Neootic3 <ilyaselhattaoui@gmail.com>
2025-09-17 18:12:58 +0100

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,26 @@
+const socket = io('http://localhost:5000');
+const messagesDiv = document.getElementById('messages');
+const msgInput = document.getElementById('msgInput');
+
+// Load chat history
+socket.on('chatHistory', (msgs) => {
+  msgs.forEach(m => addMessage(m.text));
+});
+
+// Receive new message
+socket.on('receiveMessage', (msg) => {
+  addMessage(msg.text);
+});
+
+function sendMessage() {
+  const text = msgInput.value;
+  if (!text) return;
+  socket.emit('sendMessage', text);
+  msgInput.value = '';
+}
+
+function addMessage(text) {
+  const p = document.createElement('p');
+  p.innerText = text;
+  messagesDiv.appendChild(p);
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const Datastore = require('nedb');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, { cors: { origin: '*' } });
+
+// Database
+const db = new Datastore({ filename: 'messages.db', autoload: true });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
+// Socket.IO
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  // Send chat history
+  db.find({}).sort({ createdAt: 1 }).exec((err, docs) => {
+    socket.emit('chatHistory', docs);
+  });
+
+  // Receive message
+  socket.on('sendMessage', (msg) => {
+    const message = { text: msg, createdAt: new Date() };
+    db.insert(message, () => {
+      io.emit('receiveMessage', message);
+    });
+  });
+
+  socket.on('disconnect', () => console.log('User disconnected'));
+});
+
+server.listen(5000, () => console.log('Server running on port 5000'));
